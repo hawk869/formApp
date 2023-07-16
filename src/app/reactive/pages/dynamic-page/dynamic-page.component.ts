6f5be92d521aeb2b99b237361d90d019ff13f0b9
Daniel Esteban <decepeda@misena.edu.co>
2023-07-16 16:30:48 -0500

    Insertando elementos al formArray

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -14,7 +14,8 @@ export class DynamicPageComponent {
       ['Metal Gear', Validators.required],
       ['Death Stranding', Validators.required]
     ])
-  })
+  });
+  public newFavorite: FormControl = new FormControl('', [Validators.required]);
   constructor( private fb: FormBuilder ) {
   }
   get favoriteGames() {
@@ -51,9 +52,21 @@ export class DynamicPageComponent {
       return;
     }
     console.log( this.myForm.value);
+    ( this.myForm.controls['favoriteGames'] as FormArray ) = this.fb.array([]);
     this.myForm.reset();
   }
   onDeleteFavorite( index: number): void {
     this.favoriteGames.removeAt( index );
+  }
+  addToFavorites(): void {
+    if ( this.newFavorite.invalid ) return;
+    const newGame = this.newFavorite.value;
+
+    // this.favoriteGames.push( new FormControl( newGame, Validators.required ));
+    this.favoriteGames.push(
+      this.fb.control( newGame, Validators.required )
+    );
+
+    this.newFavorite.reset();
   }
 }
