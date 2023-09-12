cfc51f0f182d963123f5a3bf964585895c07c102
Daniel Esteban <decepeda@misena.edu.co>
2023-09-12 18:36:29 -0500

    Se crea formulario en el componente register-page.component.ts

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class RegisterPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required ]],
+    email: ['', [ Validators.required ]],
+    username: ['', [ Validators.required ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]],
+  });
+
+  constructor( private fb: FormBuilder ) {
+  }
+  isValidField( field: string ) {
+    //TODO obtener validacion desde un servicio
+  }
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }
