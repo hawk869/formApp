a1de59d1e6e5bb987306e6a7e2af881de078fbee
Daniel Esteban <decepeda@misena.edu.co>
2023-09-12 16:47:52 -0500

    Se crea la logica para el formulario switches-page.component.ts

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   templateUrl: './switches-page.component.html',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   ]
 })
 export class SwitchesPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    gender: [ 'M', Validators.required ],
+    wantNotifications: [ true, Validators.required ],
+    termsAndConditions: [ true, Validators.requiredTrue ]
+  })
+  constructor( private fb: FormBuilder ) {
+  }
+  isValidField( field: string ) {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[ field ].touched;
+  }
+  onSave() {
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+  }
 
 }
