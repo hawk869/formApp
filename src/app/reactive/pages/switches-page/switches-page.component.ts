e1d82c0c6e96634b7b72a447d0d14a663c4a48c9
Daniel Esteban <decepeda@misena.edu.co>
2023-09-12 17:00:24 -0500

    Se crea un objeto person y se le asignan 2 de las 3 propiedades

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
@@ -6,15 +6,23 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
   styles: [
   ]
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
 
   public myForm: FormGroup = this.fb.group({
     gender: [ 'M', Validators.required ],
     wantNotifications: [ true, Validators.required ],
     termsAndConditions: [ true, Validators.requiredTrue ]
-  })
+  });
+  public person = {
+    gender: 'F',
+    wantNotifications: false
+  }
   constructor( private fb: FormBuilder ) {
   }
+
+  ngOnInit(): void {
+        this.myForm.reset( this.person );
+    }
   isValidField( field: string ) {
     return this.myForm.controls[field].errors
       && this.myForm.controls[ field ].touched;
@@ -24,7 +32,11 @@ export class SwitchesPageComponent {
       this.myForm.markAllAsTouched();
       return;
     }
+    const { termsAndConditions, ...newPerson } = this.myForm.value;
+
+    this.person = newPerson;
     console.log(this.myForm.value);
+    console.log(this.person);
   }
 
 }
