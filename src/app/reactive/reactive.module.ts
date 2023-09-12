b0d3b50300685009eabe7cfb45c72b686d8c4944
Daniel Esteban <decepeda@misena.edu.co>
2023-09-12 16:46:22 -0500

    Se importa el el modulo FormsModule

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -5,7 +5,7 @@ import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -15,10 +15,11 @@ import {ReactiveFormsModule} from "@angular/forms";
        DynamicPageComponent,
        SwitchesPageComponent
   ],
-  imports: [
-    CommonModule,
-    ReactiveRoutingModule,
-    ReactiveFormsModule
-  ]
+    imports: [
+        CommonModule,
+        ReactiveRoutingModule,
+        ReactiveFormsModule,
+        FormsModule
+    ]
 })
 export class ReactiveModule { }
