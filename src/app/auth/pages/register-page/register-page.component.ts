7fbc38c0e99c7e21d714e7acd675bc5d7f8b35a2
Daniel Esteban <decepeda@misena.edu.co>
2023-09-19 19:31:17 -0500

    Se utiliza validacion asincrona para los correos en el .ts

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import * as customValidators from "../../../shared/validators/validators";
 import {ValidatorsService} from "../../../shared/services/validators.service";
+import {EmailValidatorService} from "../../../shared/validators/email-validator.service";
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -12,14 +13,16 @@ export class RegisterPageComponent {
 
   public myForm: FormGroup = this.fb.group({
     name: ['', [ Validators.required, Validators.pattern( this.validatorService.firstNameAndLastnamePattern ) ]],
-    email: ['', [ Validators.required, Validators.pattern( customValidators.emailPattern ) ]],
+    // email: ['', [ Validators.required, Validators.pattern( customValidators.emailPattern ) ], [ new EmailValidatorService() ]], si la funcion es muy grande va a necesitar mas performance
+    email: ['', [ Validators.required, Validators.pattern( customValidators.emailPattern ) ], [ this.emailvalidator ]],
     username: ['', [ Validators.required, customValidators.cantBeStrider ]],
     password: ['', [ Validators.required, Validators.minLength(6) ]],
     password2: ['', [ Validators.required ]],
   });
 
   constructor( private fb: FormBuilder,
-               private validatorService: ValidatorsService ) {
+               private validatorService: ValidatorsService,
+               private emailvalidator: EmailValidatorService ) {
   }
   isValidField( field: string ) {
     return this.validatorService.isValidField( this.myForm, field )
