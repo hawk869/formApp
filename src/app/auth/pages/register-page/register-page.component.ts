135fc6e635504c64f294833373f33da7a77c554e
Daniel Esteban <decepeda@misena.edu.co>
2023-09-19 20:01:42 -0500

    Se manda llamar la funcion para validar que las contrasenas sean iguales en el .ts

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -14,15 +14,19 @@ export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
     name: ['', [ Validators.required, Validators.pattern( this.validatorService.firstNameAndLastnamePattern ) ]],
     // email: ['', [ Validators.required, Validators.pattern( customValidators.emailPattern ) ], [ new EmailValidatorService() ]], si la funcion es muy grande va a necesitar mas performance
-    email: ['', [ Validators.required, Validators.pattern( customValidators.emailPattern ) ], [ this.emailvalidator ]],
+    email: ['', [ Validators.required, Validators.pattern( customValidators.emailPattern ) ], [ this.emailValidatorService ]],
     username: ['', [ Validators.required, customValidators.cantBeStrider ]],
     password: ['', [ Validators.required, Validators.minLength(6) ]],
     password2: ['', [ Validators.required ]],
+  }, {
+    validators: [
+      this.validatorService.isFieldOneEqualFieldTwo('password', 'password2'),
+    ]
   });
 
   constructor( private fb: FormBuilder,
                private validatorService: ValidatorsService,
-               private emailvalidator: EmailValidatorService ) {
+               private emailValidatorService: EmailValidatorService ) {
   }
   isValidField( field: string ) {
     return this.validatorService.isValidField( this.myForm, field )
